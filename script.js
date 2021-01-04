a81ec425d7ba7677b375c894f4f3923ce21bc6df
unknown <Mohamedakeelkhan@gmail.com>
2021-01-04 00:00:06 -0500

    added javascript functions, addtodo, deletetodo

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const addform = document.getElementById('addForm');
+const listOfItems = document.getElementById('listOfItems');
+
+form.addEventListener('submit', addTodo);
+listOfItems.addEventListener('click', deleteTodo);
+
+function addTodo(e) {
+
+}
+
+function deleteTodo(e) {
+
+}
+
