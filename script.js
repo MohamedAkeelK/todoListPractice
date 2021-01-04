2b1bc7e22f24c9934fdb553d61bf29181ca3f146
unknown <Mohamedakeelkhan@gmail.com>
2021-01-04 00:17:22 -0500

    add todo function is now working

--- script.js
+++ script.js
@@ -2,12 +2,24 @@
 const addform = document.getElementById('addForm');
 const listOfItems = document.getElementById('listOfItems');
 
-form.addEventListener('submit', addTodo);
+addform.addEventListener('submit', addTodo);
 listOfItems.addEventListener('click', deleteTodo);
 
 function addTodo(e) {
+    e.preventDefault();
 
+    //select and get the value of the item to add
+    const itemToAdd = document.getElementById('itemToAdd').value;
+    //create a new li to store the itemToAdd
+    const newListItem = document.createElement('li');
+    //set the html equal to the itemToAdd
+    newListItem.innerHTML = itemToAdd;
+    //give new list item the same class
+    newListItem.className = "indv-item";
+    //append new list item to ul , listOfItems
+    listOfItems.appendChild(newListItem);
 }
+
 
 function deleteTodo(e) {
 
