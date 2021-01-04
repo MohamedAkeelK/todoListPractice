011f4c71d226c450b39e287c5c3f580bfefe26c3
unknown <Mohamedakeelkhan@gmail.com>
2021-01-04 01:12:16 -0500

    updated functions and added a function to check off todos

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-
+//select the form and list items(ul)
 const addform = document.getElementById('addForm');
 const listOfItems = document.getElementById('listOfItems');
 
+//event listeners
 addform.addEventListener('submit', addTodo);
 listOfItems.addEventListener('click', deleteTodo);
+listOfItems.addEventListener('click', checkTodo);
 
+//add todo function
 function addTodo(e) {
     e.preventDefault();
 
@@ -16,12 +19,33 @@ function addTodo(e) {
     newListItem.innerHTML = itemToAdd;
     //give new list item the same class
     newListItem.className = "indv-item";
-    //append new list item to ul , listOfItems
+    // create delete button
+    const deleteBtn = document.createElement('button');
+    //set the text content and classname
+    deleteBtn.textContent = "X";
+    deleteBtn.className = "delete-btn";
+    //append button to newListItem
+    newListItem.appendChild(deleteBtn);
+    //append new list item to ul , the listOfItems
     listOfItems.appendChild(newListItem);
 }
 
-
+//delete todo function
 function deleteTodo(e) {
+
+    if(e.target.classList.contains('delete-btn')) {
+        //gets the li element(parent of the btn) when delete button is clicked
+        let li = e.target.parentElement;
+        //remove the li element
+        listOfItems.removeChild(li);
+    }
 
 }
 
+//check off Todo function
+function checkTodo(e) {
+    if(e.target.classList.contains('check')) {
+        let li = e.target.parentElement;
+        li.style.textDecorationLine = "line-through";
+    }
+}
